b260d541fca85cf5af1b588c0575d6789a724574
fddr153 <felixdaza153@gmail.com>
2021-12-17 12:56:35 -0400

    Innitial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+//body container fixed NO-SCROLL
+//nav -Logo - Search -Bookmarks: recipeList
+//container: - dashboard: recipeList on Fade Scroll (No pagination)
+//           - recipeView on fade animation
+function App() {
+  return (
+    <div className="App">
+      <body></body>
+    </div>
+  );
+}
+
+export default App;
